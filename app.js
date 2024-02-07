e0ec7ccf662928d8de2d692e588d1382f7209a3f
gus0660 <guillaumeroch3@gmail.com>
2024-02-07 15:19:38 +0100

    tentative de app.delete

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.get("/archivetache", (req, res) => {
 
 //je doit poster une nouvelle tache donc avec un titre et un descriptif qui doit être "à faire"
 app.post("/tache", (req, res) => {
-  //dans ma table (database) je demande une request"req" et une reponse"res" je recois la "req"
+  //dans ma table (database) je demande une request"req" et une reponse"res"... je recois la "req" et j'emet une "res"
   const { tacheTitre, tacheContent } = req.body; //création de variable {tacheTitre et tacheContent} que je dois aller cherché dans le body de la request "req"? ENTRE {} veut dire que je veut du "json" et dans celui-ci je veu "tacheTitre et tacheContent"
   const status_idStatus = 2; //creation de variable"status_idStatus" et qui établit la valeur par défaut "2" qui correspond à "à faire" dans la table "status"
   const isFinished = 0; //création de la constante "isFinished" définie par défaut à 0
@@ -94,4 +94,15 @@ app.post("/tache", (req, res) => {
       }
     }
   );
+});
+//  je veu supprimer une tache, par exemple la derniere que j'ai envoyé, donc 
+app.delete("/tache/:id", (req, res) => {
+  const suprim = req.params.id;
+  db.query(`DELETE FROM tache WHERE idstatus = ${suprim}`, (err,results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).json({ message: "ok tache supprimé" });
+    }
+  });
 });
