c6aa473975b6c771956b51c5708e5b595d27aba4
gus0660 <guillaumeroch3@gmail.com>
2024-02-08 11:18:57 +0100

    début du fetch dans main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const àFaire = document.querySelector("#tasks");
+
+fetch('http://localhost:3000/tache')
+.then (response => response.json())
+.then (response => {
+    response.forEach(rep => {
+        
+    });
+})
