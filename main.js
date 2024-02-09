457cbd77003da84ad6b908e8b584f4c416465c17
gus0660 <guillaumeroch3@gmail.com>
2024-02-09 08:57:43 +0100

    reordonn de main.js

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-const àFaire = document.querySelector("#tasks");
 
-// fetch("http://localhost:3000/tache")
-//     .then(userFetch => userFetch.text())
-//     .then(body => console.log(body))
 fetch("http://localhost:3000/tache")
 .then(response => response.json())
 .then(body => console.log(body))
-// .catch(error => alert("Erreur : " + error));
+
+const àFaire = document.querySelector("#tasks");
