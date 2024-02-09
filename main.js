357112414a9071bad26ce735820dfe9260daf61c
gus0660 <guillaumeroch3@gmail.com>
2024-02-09 10:02:18 +0100

    commentaire

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 
-fetch("http://localhost:3000/tache")
-.then(response => response.json())
-.then(body => console.log(body))
+// fonction pour afficher une nouvelle tache rentrée dans le input et l'afficher dans le html..ne fonctionne pas pour l'instant
 
 // document.addEventListener('submit', () => {
 //     function ajouTache () {
@@ -14,5 +12,8 @@ fetch("http://localhost:3000/tache")
 // }
 // })
 
+// je veu aller chercher les taches qui sont dans la base de données et les afficher dans le html
 
-
+fetch("http://localhost:3000/tache")
+.then(response => response.json())
+.then(body => console.log(body))
