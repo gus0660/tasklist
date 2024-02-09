8b58cf50b03bebed50aad4c5fe7a667a55f69a0d
gus0660 <guillaumeroch3@gmail.com>
2024-02-09 09:57:42 +0100

    mise en commentaire de la function

--- main.js
+++ main.js
@@ -3,17 +3,16 @@ fetch("http://localhost:3000/tache")
 .then(response => response.json())
 .then(body => console.log(body))
 
-document.addEventListener('submit', () => {
-    function ajouTache () {
-    const àFaire = document.querySelector("#taskTitle").value;
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("newTache");
-    newDiv.textContent = àFaire;
-    const zoneReponse = document.querySelector("tasks");
-    zoneReponse.appendChild.àFaire;
-
-}
-})
+// document.addEventListener('submit', () => {
+//     function ajouTache () {
+//     const àFaire = document.querySelector("#taskTitle").value;
+//     let newDiv = document.createElement("div");
+//     newDiv.classList.add("newTache");
+//     newDiv.textContent = àFaire;
+//     const zoneReponse = document.querySelector("tasks");
+//     zoneReponse.appendChild.àFaire;
+// }
+// })
 
 
 
