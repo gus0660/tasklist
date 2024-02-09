57660c4a5fdfeb5201e0e052ec66fa0b7aa13204
gus0660 <guillaumeroch3@gmail.com>
2024-02-09 09:19:20 +0100

    1ere tentative de la function ajouTexte

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ fetch("http://localhost:3000/tache")
 .then(response => response.json())
 .then(body => console.log(body))
 
-
-function ajouTache () {
+document.addEventListener('submit', () => {
+    function ajouTache () {
     const àFaire = document.querySelector("#taskTitle").value;
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("newTache");
+    newDiv.textContent = àFaire;
+    const zoneReponse = document.querySelector("tasks");
+    zoneReponse.appendChild.àFaire;
 
 }
+})
+
+
+
