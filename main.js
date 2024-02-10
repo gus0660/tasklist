83a439637a255951bec67574d2f43947ce90cfb3
gus0660 <groch1@hotmail.fr>
2024-02-10 19:12:14 +0100

    fonction postTache à finir de commenter

--- main.js
+++ main.js
@@ -8,25 +8,56 @@ formEL.addEventListener("submit", (event) => {
   event.preventDefault();
   ajouTache();
 });
+//bon faut tout refaire pour envoyer la nouvelle tache dans la base de données
 
-function ajouTache() {
+
+function postTache() {
   const titreAfaire = document.querySelector("#taskTitle").value;
   const contentAfaire = document.querySelector("#taskContent").value;
-  let newDiv = document.createElement("div");
-  newDiv.style.border = "1px solid black";
-  newDiv.style.borderRadius = "10px"; // Ajout du border-radius
-  newDiv.style.margin = "10px"; // Ajout du margin
-  newDiv.style.backgroundColor = "#42ecf5";
-  let newHtrois = document.createElement("h3");
-  let newP = document.createElement("p");
-  newDiv.classList.add("newTache");
-  newHtrois.textContent = titreAfaire;
-  newP.textContent = contentAfaire;
-  newDiv.appendChild(newHtrois);
-  newDiv.appendChild(newP);
-  zoneReponse.appendChild(newDiv);
-}//bon faut tout refaire pour envoyer la nouvelle tache dans la base de données
+  // après les 2 premieres const il faut créer une const qui sera en json donc entre acolades{} regroupant les objets à envoyer
+// ces objets se composent comme suit : (colonne de la table BD) (:) (const définie avec .value)
+  const tacheData = {
+    tacheTitre: titreAfaire,
+    tacheContent: contentAfaire
+  };
+  // connection à la base de données avec un fetch et une fonction
+  fetch("http://localhost:3000/tache", {
+// on précise d'abord la méthode : 'POST'
+method: 'POST',
+// on décrit ensuite le type du header (en json {}) et le body qui est un stringify de tacheData
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  // JSON.stringify() prend un objet JavaScript et le transforme en une chaîne de texte JSON pour faciliter le partage ou le stockage des données.
+  body:JSON.stringify(tacheData)
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log('Tâche ajoutée avec succès !');
+      // Réactualisation de l'affichage des tâches depuis la base de données
+      loadTache();
+    } else {
+      console.error('Erreur lors de l\'ajout de la tâche.');
+    }
+  })
+  .catch(error => {
+    console.error('Erreur lors de la requête:', error);
+  });
 
+  // let newDiv = document.createElement("div");
+  // newDiv.style.border = "1px solid black";
+  // newDiv.style.borderRadius = "10px"; // Ajout du border-radius
+  // newDiv.style.margin = "10px"; // Ajout du margin
+  // newDiv.style.backgroundColor = "#42ecf5";
+  // let newHtrois = document.createElement("h3");
+  // let newP = document.createElement("p");
+  // newDiv.classList.add("newTache");
+  // newHtrois.textContent = titreAfaire;
+  // newP.textContent = contentAfaire;
+  // newDiv.appendChild(newHtrois);
+  // newDiv.appendChild(newP);
+  // zoneReponse.appendChild(newDiv);
+}
 loadTache();
 
 // je veu aller chercher les taches qui sont dans la base de données et les afficher dans le html
